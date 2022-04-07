f0d42e9bf61192623d56705db1eed16dbf9bcff0
iguoliveira <igor.or042@gmail.com>
2022-04-07 11:23:38 -0300

    Added the new window and inserted the JSON in select options.

--- excelShowData/ShowData.py
+++ excelShowData/ShowData.py
@@ -1,17 +1,18 @@
 from PySimpleGUI import PySimpleGUI as g
+from excelShowData.ExactData import ExactData as e
 
 class ShowData:
 
-    def showData(self):
+    def showDataWindow(self):
         g.theme('Default')
 
         show_Data = [
-            [g.Text('CLICA', key='-DATA-', enable_events=True)],
-            [g.Text('', key='new_txt')]
+            [g.Text('CLICA', key='-DATA-', enable_events=True)]
         ]
 
         layout = [
-            [g.Frame("Dados dos Alunos", layout=show_Data, key='container', size=(450, 450))]
+            [g.Button('Voltar', key='-BACK-')],
+            [g.Frame("Dados dos Alunos", layout=show_Data, key='-CONTAINER-', size=(450, 450))]
         ]
 
         window = g.Window('Mostragem dos Dados',layout=layout, size=(500,500), element_justification='c', finalize=True)
@@ -19,9 +20,12 @@ class ShowData:
         while True:
             event, value = window.read()
 
-            if event == g.WINDOW_CLOSED:
+            if event == g.WINDOW_CLOSED or event == '-BACK-':
                 break
 
             elif event == '-DATA-':
-                print("salve")
-                value['new_txt'].set_text('fuck')
+                for i in range(10):
+                    window.extend_layout(window['-CONTAINER-'], [[g.Text('salve', key=str(i), enable_events=True)]])
+
+            elif event == '1':
+                e.exactDataWindow(0)
